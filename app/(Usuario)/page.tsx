e266cec459b6d4a2acb7eef8c8b2d3c1aa3d35c2
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2023-06-06 13:25:27 +0200

    Home casi terminado

--- app/(Usuario)/page.tsx
+++ app/(Usuario)/page.tsx
@@ -2,23 +2,29 @@ import Card from '../../components/Card'
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-between p-5 overflow-y-auto">
+    <div className="flex flex-col h-screen p-5 justify-between overflow-y-auto">
       <h1 className='text-2xl font-semibold'>Herramienta de seguimiento del proyecto Mercave</h1>
       <div className="flex place-items-center">
-        <div>
-          <h2 className='text-slate-500 pt-10 pb-4'>MATERIAL RODANTE</h2>
-          <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 lg:p-0'>
-            <Card enlace="Vehiculos" titulo="Vehículos" descripcion="Geolocalización. Información y parámetros de servicio de la flota de vehículos (Locomotoras, Vehículos Auxiliares y Vagones). Mantenimientos realizados" imagen="/imagenes/vehiculos/unimog.jpg"/>
-            <Card enlace="EAVMs" titulo="Ejes EAVM" descripcion="Geolocalización, Alarmas, Telemetría de parámetros de circulación y cambios de los ejes. Mantenimientos realizados" imagen="/imagenes/ejes/eje.png"/>
+        <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-2 lg:p-0'>
+          <div>
+            <h2 className='text-slate-500 pt-10 pb-4'>MATERIAL RODANTE</h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-2'>
+              <Card enlace="Vehiculos" titulo="Vehículos" descripcion="Geolocalización. Información y parámetros de servicio de la flota de vehículos (Locomotoras, Vehículos Auxiliares y Vagones). Mantenimientos realizados" imagen="/imagenes/vehiculos/unimog.jpg"/>
+              <Card enlace="EAVMs" titulo="Ejes EAVM" descripcion="Geolocalización, Alarmas, Telemetría de parámetros de circulación y cambios de los ejes. Mantenimientos realizados" imagen="/imagenes/ejes/eje.png"/>
+            </div>
           </div>
-          <h2 className='text-slate-500 pt-10 pb-4'>CAMBIADORES</h2>
-          <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 lg:p-0'>
-            <Card enlace="#" titulo="Operaciones de cambio" descripcion="Información de operaciones de cambio realizados. Valores de los cambios. Alarmas. Mantenimientos realizados" imagen="/imagenes/varios/power.png"/>
+          <div>
+            <h2 className='text-slate-500 pt-10 pb-4'>CAMBIADORES</h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-2 h-5/6'>
+              <Card enlace="#" titulo="Operaciones de cambio" descripcion="Información de operaciones de cambio realizados. Valores de los cambios. Alarmas. Mantenimientos realizados" imagen="/imagenes/vehiculos/portacontenedores1.jpeg"/>
+            </div>
           </div>
-          <h2 className='text-slate-500 pt-10 pb-4'>GESTIÓN ADIF</h2>
-          <div className='grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 lg:p-0'>
-            <Card enlace="/Documentacion/General" titulo="Documentación Técnica" descripcion="Memorias técnicas y planos de las distintas versiones de ejes y de los vehículos" imagen="/imagenes/vehiculos/portaautos.jpg"/>
-            <Card enlace="/gestor_documentacion" titulo="Estudio" descripcion="Herramienta de Gestión Documental" imagen="/imagenes/vehiculos/portabobinas.jpg"/>
+          <div>
+            <h2 className='text-slate-500 pt-10 pb-4'>GESTIÓN ADIF</h2>
+            <div className='grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-2'>
+              <Card enlace="Documentacion/General" titulo="Documentación Técnica" descripcion="Memorias técnicas y planos de las distintas versiones de ejes y de los vehículos" imagen="/imagenes/vehiculos/portaautos.jpg"/>
+              <Card enlace="gestor_documentacion" titulo="Estudio" descripcion="Herramienta de Gestión Documental" imagen="/imagenes/vehiculos/portacontenedores2.jpg"/>
+            </div>
           </div>
         </div>
       </div>
--- components/Card.jsx
+++ components/Card.jsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function Card({enlace, titulo, descripcion, imagen}) {
 return(
 
-    <div className='border border-gray-200 m-2 rounded-lg shadow-lg overflow-hidden flex flex-col'>
+    <div className='border m-2 rounded-lg shadow-lg overflow-hidden flex flex-col'>
         <Link href={`/${enlace}`}>
-            <Image width={650} height={340} alt={titulo} src={imagen}  />
+            <Image width={650} height={650} alt={titulo} src={imagen} className="object-cover rounded-t-xl" />
             <h1 className='text-xl font-bold p-4'>{titulo}</h1>
-            <p className="text-slate-500 pl-4 pb-4">{descripcion}</p>
+            <p className="text-slate-500 pl-4 pb-4 pr-4">{descripcion}</p>
         </Link>
     </div>
 )
