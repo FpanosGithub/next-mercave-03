8f35e552297ff9ae153d92977b9885b42adbad84
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2023-06-29 18:10:38 +0200

    Loading de Banco casi terminado

--- app/(Usuario)/EAVMs/[eje]/Banco/loading.tsx
+++ app/(Usuario)/EAVMs/[eje]/Banco/loading.tsx
@@ -1,5 +1,6 @@
 import Tabs from '@/components/Tabs';
 import BreadNav from "@/components/BreadNav";
+import { BellAlertIcon } from '@heroicons/react/24/solid';
 
 export default function Loading() {
     const segmentos = {
@@ -30,15 +31,15 @@ export default function Loading() {
                         <div className='flex justify-between gap-3'>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Max</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med Tot</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         </div>
                     </div>
@@ -47,15 +48,15 @@ export default function Loading() {
                         <div className='flex justify-between gap-3'>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Max</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med Tot</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         </div>
                     </div>
@@ -64,18 +65,54 @@ export default function Loading() {
                         <div className='flex justify-between gap-3'>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. min</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         <div className='font-semibold py-1 flex flex-col'>
                             <span className='font-semibold py-1'>f. Med Tot</span>
-                            <div className="animate-pulse h-1 bg-gray-500 rounded-full mt-2 w-2"></div>
+                            <div className="animate-pulse h-1 bg-white rounded-full mt-2 w-2"></div>
                         </div>
                         </div>
                     </div>
+                </div>
+                <div className='flex flex-wrap gap-1'>
+                    <div className='pb-8 flex-1'>
+                        <div className='w-full rounded-lg bg-white border shadow-md overflow-y-auto'>
+                            <table className="min-w-full">
+                                <thead className=''>
+                                <tr className=''>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-500">
+                                    ID</th>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  px-3 py-3.5 text-left text-sm font-semibold text-gray-500">
+                                    FECHA</th>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-500 xl:table-cell">
+                                    V</th>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  hidden px-3 py-3.5 text-right text-sm font-semibold text-gray-500 xl:table-cell">
+                                    FV</th>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  hidden px-3 py-3.5 text-right text-sm font-semibold text-gray-500 md:table-cell">
+                                    SENTIDO</th>
+                                    <th scope="col" className="sticky top-0 z-10 border-b border-gray-300 bg-gray-100 bg-opacity-75 backdrop-blur backdrop-filter  px-3 py-3.5 text-right text-sm font-semibold text-gray-500">
+                                    ALARMA</th>
+                                </tr>
+                                </thead>
+                                <tbody className="divide-y divide-gray-200 bg-white">
+                                <tr className='animate pulse'>
+                                    <td className="py-4 pl-4 pr-3 text-gray-700"></td>
+                                    <td className="px-3 py-4 text-gray-700"></td>
+                                    <td className="hidden px-3 py-4 text-gray-700 xl:table-cell"></td>
+                                    <td className="hidden px-3 py-4 text-gray-700 text-right xl:table-cell"></td>
+                                    <td className="hidden px-3 py-4 text-gray-700 text-right md:table-cell"></td>
+                                    <td className="mt-4 mr-8 flex justify-end">
+                                        <BellAlertIcon className="w-6 h-6 my-1 text-gray-500"/>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>  
                 </div>
             </div>
         </div>
